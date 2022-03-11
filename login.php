660af6cd990ac7d8990e48b2185bf46b4768ec1b
Jody Winters <jodywinters11@hotmail.com>
2022-03-11 08:02:52 -0500

    Finished login backend and added route.php

--- login.php
+++ login.php
@@ -5,8 +5,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
   <title>Login</title>
-
 </head>
+
+<?php
+    include 'db_connection.php';
+
+
+?>
+
+
 <body class="h-full">
 <nav class="bg-blue-700">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -28,17 +35,22 @@
           Sign in to your account
         </h2>
       </div>
-      <form class="mt-8 space-y-6" action="" method="POST">
+      <form class="mt-8 space-y-6" action="route.php" method="POST">
         <input type="hidden" name="remember" value="true">
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
             <label for="email" class="sr-only">Email address</label>
-            <input id="email" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
+            <input id="email" name="email" type="email" autocomplete="email" value="<?php echo isset($_GET['send']) ? $_GET['send'] : ' '?>" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
           </div>
           <div>
             <label for="password" class="sr-only">Password</label>
             <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
           </div>
+          <?php
+            if($_GET['error'] == 1){
+              echo "<div class='text-red-500 text-center font-bold'>Email or password is incorrect</div>";
+            }
+          ?>
         </div>
         <p>Don't have an account? <a href="register.php" class="text-blue-600 hover:bg-gray-200">Register here.</a></p>
 

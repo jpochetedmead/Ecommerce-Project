074a00a47e6f88d06c8381840332710b02c21d6b
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2022-03-23 10:42:15 -0400

    Added head & footer templates to faqs.php and landing.php

--- faqs.php
+++ faqs.php
@@ -1,3 +1,7 @@
 <?php // TEMPLATES
+  include 'templates/head.html';
+?>
+
+<?php // TEMPLATES
   include 'templates/footer.html';
 ?>
--- landing.php
+++ landing.php
@@ -10,15 +10,6 @@ if (isset($_SESSION['ID'])) {
 <?php // TEMPLATES
   include 'templates/head.html';
 ?>
-<?php // TEMPLATES
-  include 'templates/mainMenu.html';
-?>
-<?php // TEMPLATES
-  include 'templates/sideMenu.html';
-?>
-
-<h1>Testing the page</h1>
-<p>aaaeeeiiooouuu</p>
 
 <?php // TEMPLATES
   include 'templates/footer.html';

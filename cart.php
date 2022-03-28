1442e38c56c8c2368a6fce936104be1fed59b176
Michael Baker <michaelbaker117@gmail.com>
2022-03-28 07:39:50 -0400

    update to login/register and cart pages

--- cart.php
+++ cart.php
@@ -17,78 +17,116 @@
     }
 */
 ?>
+  <nav class="bg-blue-800">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex items-center justify-between h-16">
+        <div class="flex items-center">
+          <h1 class="text-white">Ecommerce Project</h1>
+          <div>
+            <div class="ml-10 flex items-baseline space-x-4">
 
-<div id="forms">
-  <form action="home.php" method="POST" id="logo">
-    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpaperaccess.com%2Fskrillex-logo&psig=AOvVaw1sGiyQm4zClbiMB--ZRfxd&ust=1647090381536000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLDjyfuPvvYCFQAAAAAdAAAAABAv" alt="logo">
-  </form>
-<form action="home.php" method="POST" id="form2">
-  <input list="categories" name="category" placeholder="category">
-  <datalist id="categories">
-    <option value="1">
-    <option value="2">
-    <option value="3">
-    <option value="4">
-  </datalist>
-  <input type="text" name="search" placeholder="search">
-  <button type="submit" value="submit" id="productSearch">Search</button>
-</form>
+              <a href="index.php" class="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
 
-<form action="login.php" method="POST" id="form3">
-  <button type="submit" value="signin"id="signin">Login</button>
-</form>
+              <?php
+              if (isset($_SESSION['ID'])) {
+              ?>
+              <a href="logout.php" class="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Log Out</a>
+              <?php
+            } else if(!isset($_SESSION['ID'])) {
+              ?>
+              <a href="login.php" class="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Sign In</a>
 
-<form action="register.php" method="POST" id="form4">
-  <button type="submit" value="register">Sign up</button>
-</form>
+              <a href="register.php" class="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Sign Up</a>
+              <?php
+            }
+              ?>
+            </div>
+          </div>
+        </div>
+        <div>
+            <a href="cart.php" class="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                Cart
+                <span></span>
+            </a>
+            <a href="#" class="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">My Account</a>
+        </div>
+      </div>
+    </div>
+  </nav>
 
-<form action="cart.php" method="POST" id="form5">
-  <button type="submit" value="cart">Cart</button>
-</form>
-</div>
+  <!--Search bar-->
+    <div class="flex justify-center">
+        <div class="mb-3 pt-6 xl:w-3/5">
+            <form class="input-group relative flex flex-row items-stretch w-full mb-4">
+                <select class="form-select appearance-none block w-1/6 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
+                    <option value="0">All Products</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+                <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
+                <input type="submit" id="button-addon2" value="Search" class="btn inline-block px-6 py-2.5 bg-gray-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"></input>
+            </form>
+        </div>
+    </div>
+<!--shopping cart-->
+  <div class="container mx-auto mt-10">
+    <div class="flex shadow-md my-10">
+      <div class="w-3/4 bg-white px-10 py-10">
+        <div class="flex justify-between border-b pb-8">
+          <h1 class="font-semibold text-2xl">Shopping Cart</h1>
+          <h2 class="font-semibold text-2xl">1 Items</h2>
+        </div>
+        <div class="flex mt-10 mb-5">
+          <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
+          <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Quantity</h3>
+          <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Price</h3>
+          <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
+        </div>
+        <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+          <div class="flex w-2/5"> <!-- product -->
+            <div class="w-20">
+            <img src="https://via.placeholder.com/150" alt="placeholder">
+            </div>
+            <div class="flex flex-col justify-between ml-4 flex-grow">
+              <span class="font-bold text-sm">Product 1</span>
+              <span class="text-blue-500 text-xs">Brand</span>
+              <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
+            </div>
+          </div>
+          <div class="flex justify-center w-1/5">
+            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+            </svg>
 
-<?php
-session_start();
-if (isset($_SESSION['password']) && isset($_SESSION['email'])) {
-?>
-  <form action="home.php" method="POST" id="logo">
-  <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpaperaccess.com%2Fskrillex-logo&psig=AOvVaw1sGiyQm4zClbiMB--ZRfxd&ust=1647090381536000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLDjyfuPvvYCFQAAAAAdAAAAABAv" alt="logo">
-  </form>
+            <input class="mx-2 border text-center w-8" type="text" value="1">
 
-  <form action="home.php" method="POST" id="form2">
-    <input list="categories" name="category" placeholder="category">
-    <datalist id="categories">
-      <option value="1">
-      <option value="2">
-      <option value="3">
-      <option value="4">
-    </datalist>
-    <input type="text" name="search" placeholder="search">
-    <button type="submit" value="submit" id="productSearch">Search</button>
-  </form>
+            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
+              <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+            </svg>
+          </div>
+          <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>
+          <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>
+        </div>
+      </div>
 
-  <form action="logout.php" method="POST" id="form3">
-    <button type="submit" value="logout" id="logout">Logout</button>
-  </form>
+      <div id="summary" class="w-1/4 px-8 py-10">
+        <h1 class="font-semibold text-2xl border-b pb-8">Order Summary</h1>
+        <div class="flex justify-between mt-10 mb-5">
+          <span class="font-semibold text-sm uppercase">Items 1</span>
+          <span class="font-semibold text-sm">$400</span>
+        </div>
+        <div class="border-t mt-8">
+          <div class="flex font-semibold justify-between py-6 text-sm uppercase">
+            <span>Total cost</span>
+            <span>$400</span>
+          </div>
+          <button class="bg-gray-900 font-semibold hover:bg-gray-700 py-3 text-sm text-white uppercase w-full">Checkout</button>
+        </div>
+      </div>
 
-  <form action="cart.php" method="POST" id="form5">
-    <button type="submit" value="cart">Cart</button>
-  </form>
-<?php
-}
-?>
-
-<div id="Cart">
-<p>Your Cart is Empty</p>
-<form action="login.php" method="POST" id="emptyCart">
-  <button type="submit" value="signin"id="signin">Login</button>
-</form>
-
-<form action="cart.php" method="POST" id="emptyCart2">
-  <button type="submit" value="register">Sign up</button>
-</form>
-</div>
-
+    </div>
+  </div>
+</body>
 <?php // TEMPLATES
   include 'templates/footer.html';
 ?>
--- login.php
+++ login.php
@@ -32,40 +32,46 @@
       </div>
     </div>
   </nav>
-  <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div class="max-w-md w-full space-y-8">
-      <div>
-        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Sign in to your account
-        </h2>
+  <div class="mx-auto mt-10">
+    <div class="flex shadow-md my-10">
+      <div class="w-full bg-white px-10 py-10">
+        <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+          <div class="max-w-md w-full space-y-8">
+            <div>
+              <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                Sign in to your account
+              </h2>
+            </div>
+            <form class="mt-8 space-y-6" action="route.php" method="POST">
+              <input type="hidden" name="remember" value="true">
+              <div class="rounded-md shadow-sm -space-y-px">
+                <div>
+                  <label for="email" class="sr-only">Email address</label>
+                  <input id="email" name="email" type="email" autocomplete="email" value="<?php echo isset($_GET['send']) ? $_GET['send'] : ' '?>" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
+                </div>
+                <div>
+                  <label for="password" class="sr-only">Password</label>
+                  <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
+                </div>
+                <?php
+                  if($_GET['error'] == 1){
+                    echo "<div class='text-red-500 text-center font-bold'>Email or password is incorrect</div>";
+                  }
+                ?>
+              </div>
+              <div class="text-sm">
+                <a href="#" class="font-medium text-blue-600 hover:text-blue-400"> Forgot your password? </a>
+              </div>
+              <p>Don't have an account? <a href="register.php" class="text-blue-600 hover:text-blue-400">Register here.</a></p>
+              <div class="flex justify-center">
+                <button name='submit' type="submit" class="group relative w-1/2 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-900 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-      <form class="mt-8 space-y-6" action="route.php" method="POST">
-        <input type="hidden" name="remember" value="true">
-        <div class="rounded-md shadow-sm -space-y-px">
-          <div>
-            <label for="email" class="sr-only">Email address</label>
-            <input id="email" name="email" type="email" autocomplete="email" value="<?php echo isset($_GET['send']) ? $_GET['send'] : ' '?>" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
-          </div>
-          <div>
-            <label for="password" class="sr-only">Password</label>
-            <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
-          </div>
-          <?php
-            if($_GET['error'] == 1){
-              echo "<div class='text-red-500 text-center font-bold'>Email or password is incorrect</div>";
-            }
-          ?>
-        </div>
-        <div class="text-sm">
-          <a href="#" class="font-medium text-blue-600 hover:text-blue-400"> Forgot your password? </a>
-        </div>
-        <p>Don't have an account? <a href="register.php" class="text-blue-600 hover:text-blue-400">Register here.</a></p>
-        <div class="flex justify-center">
-          <button name='submit' type="submit" class="group relative w-1/2 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-900 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Sign in
-          </button>
-        </div>
-      </form>
     </div>
   </div>
 

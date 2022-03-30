c6227bf8d01e7ded6c025bd5d0b9d2608ebcf748
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2022-03-30 09:54:53 -0400

    Create selling.php file and add head/footer templates..

---
+++ selling.php
@@ -0,0 +1,34 @@
+<?php
+    include 'db_connection.php';
+?>
+
+<?php
+//TEMPLATES
+    include 'templates/head.html';
+    /*
+    switch($_SESSION['level']) {
+      case '1':
+        include 'templates/main-navbar.php';
+        break;
+      case '2':
+        include 'templates/side-menu.php';
+      default:
+        include 'templates/alert-message.html';
+    }
+*/
+?>
+
+
+
+<!--
+Two Sections For Sale | Archive
+
+Pic -> Title
+Sell Another | Share | Archive
+-->
+
+
+
+<?php // TEMPLATES
+  include 'templates/footer.html';
+?>

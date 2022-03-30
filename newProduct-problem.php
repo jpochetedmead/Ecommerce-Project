7e29bcede38a53e87c564ac03e0c94c0cc1b4f7c
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2022-03-30 10:48:30 -0400

    Update newProduct(suscess/problem) pages.

---
+++ newProduct-problem.php
@@ -0,0 +1,7 @@
+<!-- This page will appears when the seller is submiting a product, click on submit, but something went wrong... -->
+<!--
+ERROR
+POPUP ERROR message
+then:
+Try Again: (<?php header("Location: list_NewProduct.php"); ?>)
+-->
---
+++ newProduct-success.php
@@ -0,0 +1,7 @@
+<!-- This page will appears when the seller is submiting a product, click on submit, and everything goes fine...-->
+<!--
+POSTED!
+Button -> (Share to get More Views)
+Icon -> Post Another Item (<?php header("Location: list_NewProduct.php"); ?>)
+Button -> Done (<?php header("Location: selling.php"); ?>)
+-->

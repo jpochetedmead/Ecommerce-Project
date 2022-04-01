348bed9b8dd43ed9822360afc581c54f5e706980
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2022-04-01 08:22:17 -0400

    updates

--- db_connection.php
+++ db_connection.php
@@ -10,4 +10,22 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
+
+/* To show the list a product data in another page...
+
+$sql = "SELECT id, yourname, email, phone, sponsorname, sponsorshiplevel, logofile FROM Products";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+    echo "ID: " . $row["id"]. "<br>". " Name: " . $row["yourname"]. "<br>". "  Email: " . $row["email"]. "<br>". " Phone: " . $row["phone"]. "<br>". "Sponsor Name: " . $row["sponsorname"]. "<br>". "Sponsorship Level: " . $row["sponsorshiplevel"]. "<br>". "<hr>";
+}
+} else {
+  echo "0 results";
+}
+$conn->close();
+*/
+
 ?>

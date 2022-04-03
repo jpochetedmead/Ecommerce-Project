02946eb0b402373292d8d8a7de958f554cadb8c2
Michael Baker <michaelbaker117@gmail.com>
2022-04-02 20:21:44 -0400

    added user account pages

---
+++ checkout.php
@@ -0,0 +1,18 @@
+<?php
+    include 'db_connection.php';
+?>
+
+<?php
+session_start();
+?>
+
+<?php
+//TEMPLATES
+    include 'templates/head.html';
+    include 'templates/nav-bar.php';
+    include 'templates/search-bar.php';
+?>
+
+<?php // TEMPLATES
+include 'templates/footer.html';
+?>
---
+++ user_change_password.php
@@ -0,0 +1,85 @@
+<?php
+    include 'db_connection.php';
+?>
+
+<?php
+session_start();
+?>
+
+<?php
+//TEMPLATES
+    include 'templates/head.html';
+    include 'templates/nav-bar.php';
+    include 'templates/search-bar.php';
+?>
+
+<main class="flex w-full h-screen">
+    <aside class="w-80 h-screen bg-white shadow-md w-fulll">
+        <div class="flex flex-col justify-between h-screen p-4 bg-white">
+            <div class="text-sm">
+                <div class="bg-gray-900 text-white p-5 rounded">User Name</div>
+                <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">
+                    <a href="user_account.php">Personal Info</a>
+                </div>
+                <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">
+                    <a href="user_wishlist.php">Wishlist</a>
+                </div>
+                <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">
+                    <a href="user_order_history.php">Order History</a>
+                </div>
+                <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">
+                    <a href="user_change_shipping.php">Change Shipping</a>
+                </div>
+                <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">
+                    <a href="user_change_payment.php">Change Payment</a>
+                </div>
+                <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">
+                    <a href="user_change_password.php">Change Password</a>
+                </div>
+            </div>
+        </div>
+    </aside>
+
+    <section class="w-full p-4">
+        <div class="w-full text-md">
+            <div class="mt-10 sm:mt-0">
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="mt-5 md:mt-0 md:col-span-2">
+                        <form action="#" method="POST">
+                            <div class="shadow overflow-hidden sm:rounded-md">
+                                <div class="px-4 py-5 bg-white sm:p-6">
+                                    <div class="flex flex-col gap-6">
+                                        <div class="col-span-6 sm:col-span-3">
+                                            <label for="first-name" class="block text-sm font-medium text-gray-700">Old password</label>
+                                            <input id="password" name="password" type="password" class="appearance-none rounded relative block w-1/2 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
+                                        </div>
+
+                                        <div class="col-span-6 sm:col-span-3">
+                                            <label for="last-name" class="block text-sm font-medium text-gray-700">New password</label>
+                                            <input id="password" name="password" type="password" class="appearance-none rounded relative block w-1/2 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
+                                        </div>
+
+                                        <div class="col-span-6 sm:col-span-3">
+                                            <label for="last-name" class="block text-sm font-medium text-gray-700">Confirm new password</label>
+                                            <input id="conPassword" name="conPassword" type="password" class="appearance-none rounded relative block w-1/2 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
+                                        </div>
+
+                                        
+                                    </div>
+                                </div>
+                                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                    <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-900 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
+
+<?php // TEMPLATES
+include 'templates/footer.html';
+?>

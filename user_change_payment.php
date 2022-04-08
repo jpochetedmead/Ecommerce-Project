8e7a67d70a41d3b90d58a50097784ae40c373fe2
Michael Baker <michaelbaker117@gmail.com>
2022-04-08 14:38:23 -0400

    payment page updated

--- user_change_payment.php
+++ user_change_payment.php
@@ -44,45 +44,15 @@ session_start();
     </aside>
 
     <section class="w-full p-4">
-        <div class="w-full text-md">
+        <div class="w-1/2 text-md">
             <div class="mt-10 sm:mt-0">
                 <div class="md:grid md:grid-cols-3 md:gap-6">
                     <div class="mt-5 md:mt-0 md:col-span-2">
-                        <form action="#" method="POST">
-                            <div class="shadow overflow-hidden sm:rounded-md">
-                                <div class="px-4 py-5 bg-white sm:p-6">
-                                    <div class="grid grid-cols-6 gap-6">
-                                        <div class="col-span-6 sm:col-span-3">
-                                            <label for="first-name" class="block text-sm font-medium text-gray-700">First name</label>
-                                            <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
-                                        </div>
-
-                                        <div class="col-span-6 sm:col-span-3">
-                                            <label for="last-name" class="block text-sm font-medium text-gray-700">Last name</label>
-                                            <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
-                                        </div>
-
-                                        <div class="col-span-6">
-                                            <label for="email-address" class="block text-sm font-medium text-gray-700">Card number</label>
-                                            <input type="text" name="card-num" class="appearance-none rounded relative block w-1/2 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
-                                        </div>
-
-                                        <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                                            <label for="postal-code" class="block text-sm font-medium text-gray-700">ZIP / Postal code</label>
-                                            <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
-                                        </div>
-
-                                        <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                                            <label for="postal-code" class="block text-sm font-medium text-gray-700">Security code</label>
-                                            <input type="text" name="code" class="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                    <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-900 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
-                                </div>
+                        <div class="shadow sm:rounded-md">
+                            <div class="px-4 py-5 bg-white sm:p-6">
+                                <h1 class="text-lg leading-6 font-medium text-gray-900">Cash Payments Only!!</h1>
                             </div>
-                        </form>
+                        </div> 
                     </div>
                 </div>
             </div>

1fb3396f9612bf4db11fd6a3bace8819995bbb8f
Michael Baker <michaelbaker117@gmail.com>
2022-04-20 10:26:25 -0400

    added styled table for seller accounts page, needs to be merged with back end

--- sellerAccounts.php
+++ sellerAccounts.php
@@ -8,65 +8,97 @@ session_start();
 
 <?php
 include 'templates/head.html';
+include 'templates/nav-admin.php';
 ?>
 
-<nav class="bg-blue-800">
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div class="flex items-center justify-between h-16">
-      <div class="flex items-center">
-        <h1 class="text-white">Ecommerce Project</h1>
-          <div>
-            <div class="ml-10 flex items-baseline space-x-4">
-              <a href="adminHome.php" class="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
-          </div>
+<main class="flex w-full">
+  <?php
+  include 'templates/admin-side-bar.php';
+  ?>
+    <section class="w-full p-4">
+        <div class="w-full text-md">
+            <div class="bg-white shadow sm:rounded">
+                <div class="mt-5 md:mt-0 md:col-span-2">
+                    <div class="bg-white p-8 rounded-md w-full">
+                        <div class=" flex items-center justify-between pb-6">
+                            <div>
+                                <h2 class="text-gray-600 font-semibold">Seller Accounts</h2>
+                            </div>
+		                    <div class="flex items-center justify-between">
+                                <form action="buyerAccounts.php" method="POST">
+                                    <input type="submit" value="View All" name="search" class="px-5 py-5 border-b border-gray-200 bg-white cursor-pointer text-xs text-gray-600 font-semibold hover:text-gray-400">
+                                </form>
+                            </div>
+                        </div>
+
+                        <!--Table-->
+                        <form action="buyerAccounts.php" id="form1" method="POST">
+                            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                                <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                                    <!--Table Header-->
+                                    <table class="min-w-full leading-normal">
+                                        <thead>
+                                            <tr>
+                                            <th
+                                                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                                Name
+                                            </th>
+                                            <th
+                                                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                                Email
+                                            </th>
+                                            <th
+                                                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                                ID
+                                            </th>
+                                            <th
+                                                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                                Phone Number
+                                            </th>
+                                            </tr>
+                                        </thead>
+                                        <!--Table Body-->
+                                        <tbody>
+                                            <tr>
+                                            <!--Name-->
+                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                <p class="text-gray-900 whitespace-no-wrap">
+                                                Random text:
+                                                </p>
+                                            </td>
+                                            <!--Email-->
+                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                <p class="text-gray-900 whitespace-no-wrap">
+                                                Random text: 
+                                                </p>
+                                            </td>
+                                            <!--ID-->
+                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                <p class="text-gray-900 whitespace-no-wrap">
+                                                Random text: 
+                                                </p>
+                                            </td>
+                                            <!--Phone Number-->
+                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                <p class="text-gray-900 whitespace-no-wrap">
+                                                Random text: 
+                                                </p>
+                                            </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </form>
+                        <!------>
+	                </div>
+                </div>
+            </div>
         </div>
-      </div>
+    </section>
+</main>
 
-      <div>
-        <?php
-        if (isset($_SESSION['ID'])) {
-        ?>
-        <a href="logout.php" class="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Log Out</a>
-        <?php
-      }
-        ?>
-      </div>
-    </div>
-  </div>
-</nav>
-
-  <main class="flex w-full h-screen">
-    <aside class="w-80 h-screen bg-white shadow-md w-fulll">
-      <div class="flex flex-col justify-between h-screen p-4 bg-white">
-        <div class="text-sm">
-          <div class="bg-gray-900 text-white p-3 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300 font-medium">
-            <a href="buyerAccounts.php">Buyer Accounts</a>
-          </div>
-          <div class="bg-gray-900 text-white p-3 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300 font-medium">
-            <a href="sellerAccounts.php">Seller Accounts</a>
-          </div>
-          <div class="bg-gray-900 text-white p-3 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300 font-medium">
-            <a href="inventory.php">Inventory</a>
-          </div>
-          <div class="bg-gray-900 text-white p-3 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300 font-medium">
-            <a href="editCategories.php">Edit Categories</a>
-          </div>
-          <div class="bg-gray-900 text-white p-3 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300 font-medium">
-            <a href="approval.php">Account Approval</a>
-          </div>
-          <div class="bg-gray-900 text-white p-3 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300 font-medium">
-            <a href="disabledAccounts.php">Disabled Accounts</a>
-          </div>
-        </div>
-      </div>
-    </aside>
-
-  <h1>Seller Accounts</h1>
-
-  <form action="sellerAccounts.php" method="POST">
-    <input type="submit" value="View All" name="search">
-  </form>
-
+<!---This needs to merged with the table above--->
 <?php
 if(isset($_POST['search'])){
   $search = $_POST['search'];
@@ -76,12 +108,6 @@ if(isset($_POST['search'])){
   $res = $result->fetch_assoc();
 ?>
   <table>
-    <tr>
-      <th>Name</th>
-      <th>Email</th>
-      <th>ID</th>
-      <th>Phone Number</th>
-    </tr>
 
 <?php
   while($res = mysqli_fetch_array($result)) {
@@ -96,3 +122,7 @@ if(isset($_POST['search'])){
 }
 ?>
 </table>
+
+<?php
+include 'templates/footer.html';
+?>

7686be76690f934dafad0f4e53790d7927a95573
Michael Baker <michaelbaker117@gmail.com>
2022-04-20 08:48:19 -0400

    added side bar for admin pages

---
+++ templates/admin-side-bar.php
@@ -0,0 +1,24 @@
+    <aside class="w-80 bg-white shadow-md w-fulll">
+        <div class="flex flex-col justify-between h-screen p-4 bg-white">
+            <div class="text-sm">
+                <div class="bg-gray-900 text-white p-3 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300 font-medium">
+                    <a href="buyerAccounts.php">Buyer Accounts</a>
+                </div>
+                <div class="bg-gray-900 text-white p-3 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300 font-medium">
+                    <a href="#">Seller Accounts</a>
+                </div>
+                <div class="bg-gray-900 text-white p-3 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300 font-medium">
+                    <a href="#">Inventory</a>
+                </div>
+                <div class="bg-gray-900 text-white p-3 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300 font-medium">
+                    <a href="#">Edit Categories</a>
+                </div>
+                <div class="bg-gray-900 text-white p-3 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300 font-medium">
+                    <a href="approval.php">Account Approval</a>
+                </div>
+                <div class="bg-gray-900 text-white p-3 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300 font-medium">
+                    <a href="#">Disabled Accounts</a>
+                </div>
+            </div>
+        </div>
+    </aside>

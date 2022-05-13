ff0500c57b4542597294ff32fa1b3058df894c3a
Jody Winters <jodywinters11@hotmail.com>
2022-05-13 08:16:13 -0400

    Added backend to payouts and order pages

--- order.php
+++ order.php
@@ -1,9 +1,10 @@
 <?php
     include 'db_connection.php';
     session_start();
-?>
 
-<?php
+
+
+
 //TEMPLATES
     include 'templates/head.html';
     include 'templates/nav-bar.php';
@@ -18,34 +19,60 @@
       <div class="w-full text-md">
           <div class='bg-white shadow sm:rounded'>
             <div class='px-4 py-5 sm:px-6'>
-              <h3 class='text-lg leading-6 font-medium text-gray-900'>Order number: </h3>
+              <h3 class='text-lg leading-6 font-medium text-gray-900'>Order number: <?php echo $_GET['trans'] ?></h3>
             </div>
+            <?php
+            $sql = "SELECT * FROM Transactions WHERE transaction_ID=$_GET[trans] LIMIT 1";
+            $result = $conn->query($sql);
+            $account = $result->fetch_assoc();
+            $id = $account['user_ID'];
+
+            $sql = "SELECT * FROM Users WHERE user_ID = $id";
+            $resu = $conn->query($sql);
+            $user = $resu->fetch_assoc();
+            ?>
             <div class='border-t border-gray-200'>
               <dl>
                 <div class='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
                     <dt class='text-sm font-medium text-gray-500'>Full name</dt>
-                    <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>firstName lastName</dd>
+                    <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'><?php echo $user['first_name'] . " " . $user['last_name'] ?></dd>
                 </div>
                 <div class='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
                     <dt class='text-sm font-medium text-gray-500'>Shipping address</dt>
-                    <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>addressFirstLine addressSecondLine city, state zip</dd>
+                    <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'><?php echo $user['address_first_line'] ?></dd>
+                    <dt class='text-sm font-medium text-gray-500'> </dt>
+                    <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'><?php echo $user['address_second_line'] ?></dd>
+                    <dt class='text-sm font-medium text-gray-500'> </dt>
+                    <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'><?php echo $user['city'] . ", " . $user['state_abbreviation'] . " " . $user['zip_code'] ?></dd>
                 </div>
+                
                 <div class='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
                     <dt class='text-sm font-medium text-gray-500'>Items purchased</dt>
                     <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
                     <div>
-                        <p>items</p>
+                    <?php
+                    $shipped = 0;
+                    $total = 0;
+                    $sql = "SELECT * FROM Transactions t INNER JOIN products p ON t.product_ID = p.product_ID WHERE t.transaction_ID=$_GET[trans]";
+                    $count = $conn->query($sql);
+                    while($row = mysqli_fetch_array($count)) {
+                        if($row['seller_ID'] == $_SESSION['ID']){
+                            if($row['shipped'] == 1) $shipped = 1;
+                            $total += ($row['product_quantity'] * $row['price_at_purchase']);
+                            echo "<p>" . $row['product_quantity'] . "X " . $row['title'] . "</p>";
+                        }
+                    }
+                    ?>
                     </div>
-
                     </dd>
                 </div>
                 <div class='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
                     <dt class='text-sm font-medium text-gray-500'>Total payment</dt>
-                    <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>Price </dd>
+                    <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>$<?php echo sprintf("%.2f", $total) ?></dd>
                 </div>
                 <div class='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
                     <dt class='text-sm font-medium text-gray-500'>Status</dt>
-                    <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>Processing</dd>
+                    <dd class='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'><?php echo (($shipped == 1)? 'Shipped':'Processing') ?></dd>
                 </div>
               </dl>
             </div>
--- seller_payouts.php
+++ seller_payouts.php
@@ -1,12 +1,31 @@
 <?php
-    include 'db_connection.php';
-?>
-
-<?php
+include 'db_connection.php';
 session_start();
-?>
 
-<?php
+
+if(isset($_POST['transfer'])) {
+    $arr = explode(' ',$_POST['transfer']);
+    $trans = $arr[0];
+    $ID = $arr[1];
+
+    $sql="SELECT * FROM Transactions WHERE transaction_ID=$trans";
+    $result = $conn->query($sql);
+    while($res = mysqli_fetch_array($result)) {
+        $sql = "SELECT * FROM products WHERE product_ID=$res[product_ID]";
+        $resu = $conn->query($sql);
+        $check = $resu->fetch_assoc();
+        if($check['seller_ID'] == $ID){
+            $sql = "UPDATE Transactions SET payout = 1 WHERE transaction_ID=$trans AND product_ID=$res[product_ID]";
+            if ($conn->query($sql) === TRUE) {
+                echo "Record updated successfully";
+            }else{
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        }
+    }
+
+}
+
 include 'templates/head.html';
 include 'templates/nav-bar.php';
 include 'templates/search-bar.php';
@@ -40,7 +59,7 @@ include 'templates/search-bar.php';
                                             </th>
                                             <th
                                                 class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                                Name
+                                                Customer Name
                                             </th>
                                             <th
                                                 class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
@@ -53,39 +72,74 @@ include 'templates/search-bar.php';
                                             </tr>
                                         </thead>
                                         <!--Table Body-->
+                                        <form action="seller_payouts.php" id="form1" method="POST">
                                         <tbody>
+                                        <?php
+                                        $sql="SELECT * FROM Transactions t INNER JOIN products p on t.product_ID=p.product_ID  WHERE p.seller_ID=$_SESSION[ID] GROUP BY transaction_ID ORDER BY purchase_date DESC";
+                                        $result = $conn->query($sql);
+
+                                        while($res = mysqli_fetch_array($result)) {
+                                            $total = 0;
+
+                                            
+                                            $sql = "SELECT * FROM Users WHERE user_ID=$res[user_ID]";
+                                            $resu = $conn->query($sql);
+                                            $t = $resu->fetch_assoc();
+                                        ?>
                                             <tr>
                                             <!--Order number-->
                                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                <a href="order.php" class="text-blue-500 hover:text-blue-300 whitespace-no-wrap">
-                                                Random text:
+                                                <a href="order.php?trans=<?php echo $res['transaction_ID']?>" class="text-blue-500 hover:text-blue-300 whitespace-no-wrap">
+                                                <?php echo $res['transaction_ID'] ?>
                                                 </a>
                                             </td>
                                             <!--Name-->
                                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                                 <p class="text-gray-900 whitespace-no-wrap">
-                                                Random text:
+                                                <?php echo $t['first_name'] . " " . $t['last_name'] ?>
                                                 </p>
                                             </td>
+
+                                            <?php
+
+                                            $sql = "SELECT * FROM Transactions t INNER JOIN products p ON t.product_ID = p.product_ID WHERE t.transaction_ID=$res[transaction_ID]";
+                                            $count = $conn->query($sql);
+                                            while($row = mysqli_fetch_array($count)) {
+                                                if($row['seller_ID'] == $_SESSION['ID']){
+                                                    $total += ($row['product_quantity'] * $row['price_at_purchase']);
+                                                }
+                                            }
+
+                                            ?>
                                             <!--Payout-->
                                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                <p class="text-gray-900 whitespace-no-wrap">
-                                                $400
+                                                <p class="text-gray-900 whitespace-no-wrap">$
+                                                <?php echo sprintf("%.2f", $total) ?>
                                                 </p>
                                             </td>
-                                            <td class="px-5 py-5 border-b border-gray-200 bg-white flex justify-evenly">
-                                                <div class="flex items-center">
-                                                    <input name="shipped[]" value="shipped" type="checkbox" class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500">
-                                                    <label for="shipped" class="ml-3 text-sm text-gray-600">Shipped</label>
-                                                </div>
+                                            <td class="px-5 py-5 border-b border-gray-200 bg-white">
+                                            <?php
+                                                if($res['payout'] == 0){
+                                                   ?>
+                                                    <div class="flex items-center">
+                                                        <button type="submit" form="form1" value='<?php echo $res['transaction_ID'] . " " . $res['seller_ID'] ?>' name="transfer" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-900 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Transfer Funds</button>
+                                                    </div>
+                                                   <?php
+                                                }else{
+                                                   ?>
+                                                    <p>Transferred</p>
+                                                   <?php
+                                                }
 
-                                                <div class="flex items-center">
-                                                    <input name="processing[]" value="processing" type="checkbox" class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500">
-                                                    <label for="processing" class="ml-3 text-sm text-gray-600">Processing</label>
-                                                </div>
+                                            ?>
+                                                
                                             </td>
                                             </tr>
+                                        <?php
+                                        }
+                                        ?>
                                         </tbody>
+                                        </form>
                                     </table>
                                 </div>
                             </div>

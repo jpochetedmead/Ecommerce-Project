a4e58c2fa05dd28338bd81979203fd3a4d089ffe
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2022-05-17 13:29:45 -0400

    updated database file.

--- db_connection.php
+++ db_connection.php
@@ -11,8 +11,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-
 /* To show the list a product data in another page...
 $sql = "SELECT id, yourname, email, phone, sponsorname, sponsorshiplevel, logofile FROM Products";
 $result = $conn->query($sql);
